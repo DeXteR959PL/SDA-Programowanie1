938863e7e3c04549df3e383195f0250841934ddb
Krystian Jakubczyk <jakubczyk.krystiann@gmail.com>
2018-03-18 09:52:49 +0100

    Queue i Stack

---
+++ src/pl/sdacademy/Stack/Stack.java
@@ -0,0 +1,50 @@
+package pl.sdacademy.Stack;
+
+/**
+ * Kolejka LIFO (Last In First Out) - Stos
+ */
+public class Stack<T> {
+    private final static int SIZE = 5;
+    private Object[] stack;
+    private int index;
+
+    public Stack() {
+        stack = new Object[SIZE];
+        index=-1;
+    }
+
+    public void push(T i) {
+        if(index >= stack.length-1) {
+            extendStack();
+        }
+        stack[++index] = i;
+    }
+
+    private void extendStack() {
+        Object[] newStack = new Object[stack.length + SIZE];
+        for (int i = 0; i < stack.length; i++) {
+            newStack[i]=stack[i];
+        }
+        stack = newStack;
+    }
+
+    public T pop() {
+        return (T) stack[index--];
+    }
+
+    public T peek() {
+        return (T) stack[index];
+    }
+
+    public int size() {
+        return index;
+    }
+
+    public void print(){
+        for (int i =index; i>= 0; i--) {
+//            System.out.println("-----");
+            System.out.println("| " + stack[i]+" |");
+        }
+//        System.out.println("-----");
+    }
+}
---
+++ src/pl/sdacademy/Stack/StackTest.java
@@ -0,0 +1,33 @@
+package pl.sdacademy.Stack;
+
+import pl.sdacademy.Stack.Stack;
+
+public class StackTest {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.push(9);
+        stack.push(10);
+
+        stack.print();
+
+        System.out.println("Zdejmuje: " + stack.pop());
+
+        stack.print();
+
+
+        Stack<String> stackText = new Stack<String>();
+        stackText.push("Ala");
+        stackText.push("ma");
+        stackText.push("kota ");
+        stackText.print();
+
+        System.out.println("Zdejmuje: " + stackText.pop());
+
+        stackText.print();
+    }
+}
